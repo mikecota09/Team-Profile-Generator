c0d8b455565a6668b8f3788890cd511a32518e0e
Michael Cota <mikecotahf@gmail.com>
2022-04-08 17:41:00 -0400

    added intern questions to index.js and updated managerquestions in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const questions = () => {
+const managerQuestions = () => {
     inquirer.prompt([
         {
             type: 'input',
@@ -35,6 +35,8 @@ const questions = () => {
             case 'Engineer':
                 engineerQuestions();
                 break;
+            case 'Intern':
+                internQuestions();
         }
     })
 };
@@ -59,9 +61,49 @@ const engineerQuestions = () => {
         {
             type: 'input',
             name: 'engineerGithub',
-            message: 'What is the engineer\'s GitHub username?'
+            message: 'What is the engineer\'s GitHub username?',
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'What type of team member would you like to add next?',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
         }
     ])
+    .then((engineer) => {
+        console.log(engineer)
+    })
 };
 
-questions();
+const internQuestions = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: 'What is the intern\'s name?',
+        },
+        {
+            type: 'input',
+            name: 'internID',
+            message: 'What is the intern\'s id?',
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: 'What is the intern\'s email address?',
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: 'What is the intern\'s school?',
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'What type of team member would you like to add next?',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
+        }
+    ])
+}
+
+managerQuestions();
